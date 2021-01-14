fd42496bf63e69a99cef90e6c2e97b70cd1c3f9d
Allyson Camitta <allyson.camitta@gmail.com>
2021-01-14 09:01:17 -0500

    feat redux store all songs

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,13 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import songs from './songs'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user,
+  songs
+})
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
