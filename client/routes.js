3f35e6503a14d5dec288bd8dc414a6c3328606a9
Kathy Morawski <kathymorawski@gmail.com>
2021-01-15 13:08:09 -0600

    Chore: Added the Song import in my route.js file to resolve my merge conflict

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Single_Song, Cart} from './components'
+import {Login, Signup, UserHome, Single_Song, Songs, Cart} from './components'
 import {me} from './store'
 
 /**
