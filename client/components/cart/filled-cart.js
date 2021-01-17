1dbcb20b76d0e3b408e99b282a2eb312dbdfd249
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-01-17 09:31:30 -0500

    fix: adding dummy data for cart component, so that it can render the list of mixtapes and quantity of items in the cart

--- client/components/cart/filled-cart.js
+++ client/components/cart/filled-cart.js
@@ -7,12 +7,112 @@ export class FilledCart extends Component {
   }
 
   render() {
-    const mixtapes = this.props.mixtapes
+    // mock data will be removed when the BE issue is fixed: https://github.com/daring-durian/mixtape-4-u/issues/59
+    const mixtapes = [
+      {
+        id: 5,
+        medium: 'vinyl',
+        name: 'Workout Mixtape',
+        price: null,
+        createdAt: '2021-01-17T13:53:35.232Z',
+        updatedAt: '2021-01-17T13:53:35.254Z',
+        orderId: 4,
+        songs: [
+          {
+            id: 3,
+            name: 'This Year',
+            artist: 'The Mountain Goats',
+            album: 'The Sunset Tree',
+            year: '2005',
+            tags: 'rock',
+            imageUrl: '/album-art/AKAA.png',
+            songUrl:
+              'https://open.spotify.com/embed/track/0s9aeZriwqyBYfxFzsd20R?si=dN0eNchlQKOtF-7S0Sslcg',
+            createdAt: '2021-01-17T13:53:35.210Z',
+            updatedAt: '2021-01-17T13:53:35.210Z',
+            songs_mixtapes: {
+              createdAt: '2021-01-17T13:53:35.305Z',
+              updatedAt: '2021-01-17T13:53:35.305Z',
+              songId: 3,
+              mixtapeId: 5
+            }
+          },
+          {
+            id: 4,
+            name: 'September',
+            artist: 'Earth, Wind & Fire',
+            album: 'The Best of Earth, Wind & Fire Vol. 1',
+            year: '1978',
+            tags: 'disco',
+            imageUrl: '/album-art/AKAA.png',
+            songUrl:
+              'https://open.spotify.com/embed/track/2grjqo0Frpf2okIBiifQKs?si=R8EcGX-PTvyYlbcVVDN70g',
+            createdAt: '2021-01-17T13:53:35.210Z',
+            updatedAt: '2021-01-17T13:53:35.210Z',
+            songs_mixtapes: {
+              createdAt: '2021-01-17T13:53:35.307Z',
+              updatedAt: '2021-01-17T13:53:35.307Z',
+              songId: 4,
+              mixtapeId: 5
+            }
+          }
+        ]
+      },
+      {
+        id: 3,
+        medium: 'vinyl',
+        name: 'Yuliyas Mixtape',
+        price: null,
+        createdAt: '2021-02-17T13:53:35.232Z',
+        updatedAt: '2021-02-17T13:53:35.254Z',
+        orderId: 5,
+        songs: [
+          {
+            id: 3,
+            name: 'This Year',
+            artist: 'The Mountain Goats',
+            album: 'The Sunset Tree',
+            year: '2005',
+            tags: 'rock',
+            imageUrl: '/album-art/AKAA.png',
+            songUrl:
+              'https://open.spotify.com/embed/track/0s9aeZriwqyBYfxFzsd20R?si=dN0eNchlQKOtF-7S0Sslcg',
+            createdAt: '2021-01-17T13:53:35.210Z',
+            updatedAt: '2021-01-17T13:53:35.210Z',
+            songs_mixtapes: {
+              createdAt: '2021-01-17T13:53:35.305Z',
+              updatedAt: '2021-01-17T13:53:35.305Z',
+              songId: 3,
+              mixtapeId: 5
+            }
+          },
+          {
+            id: 4,
+            name: 'September',
+            artist: 'Earth, Wind & Fire',
+            album: 'The Best of Earth, Wind & Fire Vol. 1',
+            year: '1978',
+            tags: 'disco',
+            imageUrl: '/album-art/AKAA.png',
+            songUrl:
+              'https://open.spotify.com/embed/track/2grjqo0Frpf2okIBiifQKs?si=R8EcGX-PTvyYlbcVVDN70g',
+            createdAt: '2021-01-17T13:53:35.210Z',
+            updatedAt: '2021-01-17T13:53:35.210Z',
+            songs_mixtapes: {
+              createdAt: '2021-01-17T13:53:35.307Z',
+              updatedAt: '2021-01-17T13:53:35.307Z',
+              songId: 4,
+              mixtapeId: 5
+            }
+          }
+        ]
+      }
+    ]
+    //const mixtapes = this.props.mixtapes
     const totalItemCount = mixtapes.length
-    // total will be calculated once we miplement add to cart functionality
+    // total will be calculated once we implement add to cart functionality
     const total = null
     const shipping = 'FREE'
-    console.log(this.props.mixtapes)
 
     return (
       <>
@@ -25,7 +125,16 @@ export class FilledCart extends Component {
               <Card.Header>
                 <h2>Cart ({totalItemCount} items)</h2>
               </Card.Header>
-              <Card.Body>{/* List of mixtapes will go here */}</Card.Body>
+              <Card.Body>
+                {mixtapes.map(mixtape => (
+                  <>
+                    {/* Mixtape list component will go here */}
+                    <Card>
+                      <Card.Body>Mixtape card will be rendered here</Card.Body>
+                    </Card>
+                  </>
+                ))}
+              </Card.Body>
             </Card>
 
             <Card className="mt-5">
