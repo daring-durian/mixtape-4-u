7d956afb54ef7ed07848e7a2e3d72baeac4c66ec
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-01-17 10:09:03 -0500

    removing unused imports

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
-import {Alert, Button, Card, Container, Form, InputGroup} from 'react-bootstrap'
+import {Button, Card, Container, Form, InputGroup} from 'react-bootstrap'
 
 /**
  * COMPONENT
