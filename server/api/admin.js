852a1ce2debee0adbaf2244b5f7af12d8ce18a3e
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-01-19 15:27:44 -0500

    feature: added admin delete/song route, fixed 500 on logout that was caused by localstorage removal

--- server/api/admin.js
+++ server/api/admin.js
@@ -1,18 +1,93 @@
 const router = require('express').Router()
 module.exports = router
 
-const {User} = require('../db/models')
+const {User, Song} = require('../db/models')
 
-// api/admin
+// api/admin returns all user data
 router.get('/', async (req, res, next) => {
-  // gets all user information
-
   const isAdmin = req.user.role === 'admin'
 
   try {
     if (isAdmin) {
       const allUserData = await User.findAll()
       res.send(allUserData)
+    } else {
+      res.send(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// api/admin/create/song enables admin to modify song data song data
+// accepts following schema
+// {
+//   "name": "Don't You Want Me",
+//   "artist": "The Human League",
+//   "album": "Dare!",
+//   "year": "1981",
+//   "tags": "pop",
+//   "imageUrl": "https://upload.wikimedia.org/wikipedia/en/c/ce/Dare-cover.png",
+//   "songUrl": "https://open.spotify.com/embed/track/3L7RtEcu1Hw3OXrpnthngx?si=_ufp6czURLGFswoYFV52-Q"
+// }
+
+router.post('/create/song', async (req, res, next) => {
+  const isAdmin = req.user.role === 'admin'
+  const newSong = req.body
+
+  try {
+    if (isAdmin) {
+      await Song.create(newSong)
+      res.send(200).end()
+    } else {
+      res.send(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// api/admin/edit/song/:songId
+// router.put('/edit/song/:songId', async (req, res, next) => {
+
+//   const isAdmin = req.user.role === 'admin'
+//   const songId = req.params.songId
+//   const songData =
+
+//   try {
+//     if (isAdmin){
+//       await Song.destroy({
+//         where: {
+//           id: songId
+//         }
+//       })
+//       res.send(200).end()
+//     }
+
+//     else {
+//       res.send(404)
+//     }
+//   }
+
+//   catch (error) {
+//     next(error)
+//   }
+// })
+
+// api/admin/delete/song/songId
+// accepts just the ID of the song in request URL
+router.delete('/delete/song/:songId', async (req, res, next) => {
+  const isAdmin = req.user.role === 'admin'
+  const songId = req.params.songId
+
+  try {
+    if (isAdmin) {
+      await Song.destroy({
+        where: {
+          id: songId
+        }
+      })
+      res.send(200).end()
     } else {
       res.send(404)
     }
